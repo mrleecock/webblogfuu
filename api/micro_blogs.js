2bce37e357dcefe669f53ce4e285953df7944a68
mrleecock <46054088+mrleecock@users.noreply.github.com>
2020-07-30 11:33:07 +0300

    Create micro_blogs.js

---
+++ api/micro_blogs.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require('mongoose');
+
+const Mblog = require('./models/mblog');
+
+
+
+// get all micro blogs
+router.get("/", (req, res) => {
+  Mblog.find(function () {
+		if (err) return console.error(err);
+		res.send('db action ok');	
+  });
+});
+
+router.post("/", (req, res) => {
+  const mblog = new Mblog({
+  	blog_id: mongoose.Types.ObjectId(), 
+  	author: req.body.author,
+   blog_text: req.body.blog_text
+  });
+  
+
+  if (!mblog.author || !mblog.blog_text) {
+    return res.status(400).json({ msg: "Error" });
+  }
+	mblog.save(function (err){
+		if (err) return console.error(err);
+		res.send('db action ok');	
+	});
+
+});
+
+module.exports = router;
